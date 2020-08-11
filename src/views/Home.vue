32a9062c81d69958a73d0fff83b5678cb98a5a49
jiwoo <pikaroc@gmail.com>
2020-08-11 15:27:07 +0900

    set env

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
     <h1>this is home</h1>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
+  // components: {
+  // }
 };
 </script>
