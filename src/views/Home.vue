749ef27c8c46aae1a1a194717b38902f650e0187
whatdidusay <pikaroc@gmail.com>
2020-08-13 22:02:40 +0900

    path fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,75 +39,75 @@ export default {
       list: [
         {
           title: "외관 품질",
-          img: "../../public/exterior.png",
+          img: "./public/exterior.png",
           part: "조립상태, 단차, 스크래치, 도색불량",
           description:
             "도장면, 창문 유리면에 스크래치가 있는지 탁송중 돌맹이에 맞지 않았는지 확인!!"
         },
         {
           title: "타이어",
-          img: "../../public/wheel.png",
+          img: "./public/wheel.png",
           part: "제조일, 휠 밸런스 확인 스티커, 휠 스크래치",
           description:
             "제조기간이 6개월 이내인지 확인하고 주황색 스티커 유무 꼭 확인! 차량 출고시 신차에는 무조건 스티커가 붙어있음 휠의 크기가 동일한지 확인!"
         },
         {
           title: "내부 품질",
-          img: "../../public/sheet.png",
+          img: "./public/sheet.png",
           part: "시트마감, 고무패킹, 도어 개폐 상태",
           description:
             "개폐시 이상 소음은 없는지 손잡이와 잠금장치는 멀쩡한지 선루프가 있다면 선루프 작동확인, 창문 열고 닫고 해보기, 고무패킹과 잘 안맞으면 비가 새니까 꼭 확인!"
         },
         {
           title: "센터페시아, 콘솔 품질",
-          img: "../../public/interior.png",
+          img: "./public/interior.png",
           part: "조립상태, 편의장치",
           description:
             "운전석에 앉아서 콘솔, 센터페시아등 버튼들을 눌러보아요 버튼 상태는 괜찮은지 에어컨, 히터 제대로 작동 확인 공조기 바람방향, 냄새, 오디오 음질 확인! 주행거리 6~8km내외인데 이보다 많으면 인수거부차량이거나 출고과정중 사고차량은 아닌지 의심해보자!"
         },
         {
           title: "스티어링 휠",
-          img: "../../public/steering-wheel.png",
+          img: "./public/steering-wheel.png",
           part: "스티어링 휠 방향",
           description:
             "타이어를 정방향을 맞추고 스티어링휠이 중앙에 맞는지 확인! 임시운행이 가능하다면 스티어링 휠이 중앙에 맞춰 직진하는지 확인!"
         },
         {
           title: "엔진룸 상태",
-          img: "../../public/engine.png",
+          img: "./public/engine.png",
           part: "엔진오일, 미션오일, 브레이크오일, 냉각수, 클램프 호스",
           description:
             "엔진룸이 청결한지 클램프가 연결이 제대로 되어있는지 냉각수 누수는 없는지 확인!"
         },
         {
           title: "엔진 상태",
-          img: "../../public/cluster.png",
+          img: "./public/cluster.png",
           part: "엔진, 시동",
           description:
             "3~5회 시동을 껏다 켰다 체크, 모든 기기를 끄고 시동을 켠후 예열되는 시간동안 떨림이나 소음은 없는지 확인!"
         },
         {
           title: "조명장치 내부 품질",
-          img: "../../public/roomlamp.png",
+          img: "./public/roomlamp.png",
           part: "내부 조명 확인",
           description:
             "각종 버튼의 조명, 브레이크등, 상하향등, 좌우 비상등 확인!"
         },
         {
           title: "조명장치 외부 품질",
-          img: "../../public/led.png",
+          img: "./public/led.png",
           part: "외부 조명 확인",
           description: "조사각은 맞는지 방향지시등, 안개등, 테일램프등 확인!"
         },
         {
           title: "차량 지급품",
-          img: "../../public/gift.png",
+          img: "./public/gift.png",
           part: "스페어타이어 ,타이어 수리키트, 소화기, 삼각대 등",
           description: "트렁크를 열어서 차량 기본 지급품목, 공구들 확인!"
         },
         {
           title: "인수 서류 확인",
-          img: "../../public/paper.png",
+          img: "./public/paper.png",
           part: "임시운행허가증, 세금계산서, 차량인수증, 차량등록증, 차대번호",
           description:
             "차량등록증의 차대번호와 실제 차량의 차대번호가 일치하는지 반드시 확인!"
