cebf6f9f7d7f6bd275ab1f0a11ca547ff16a1767
whatdidusay <pikaroc@gmail.com>
2020-12-21 01:06:08 +0900

    check car page buildin

---
+++ src/views/CheckCar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="checkCar-wrap">
+    <h1>새차삿당! 신차검수하자 :D</h1>
+    <ul class="list-wrap">
+      <li v-for="(item, idx) in list" :key="idx" @change="open()">
+        <label>
+          <input type="checkbox" :value="idx" v-model="checkNumb" />
+          <span class="check-btn"></span>
+          <div class="item-wrap">
+            <p class="title">{{ idx + 1 }}.&nbsp;{{ item.title }}</p>
+            <img :src="item.img + '.png'" alt="위치별 이미지" />
+            <p class="part">{{ item.part }}</p>
+            <p class="des">{{ item.description }}</p>
+          </div>
+        </label>
+      </li>
+    </ul>
+    <div class="status">
+      <b>{{ checkNumb.length }}&nbsp;</b>
+      <span>/&nbsp;{{ list.length }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkCar",
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/Complete.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dim" @click="close()" v-show="modal">
+    <img src="../../public/party.png" alt="폭죽 배경" class="modal-bg" />
+    <div class="modal">
+      <p>조심조심 살살타여!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "checkCar",
+  data() {
+    return {};
+  },
+};
+</script>
